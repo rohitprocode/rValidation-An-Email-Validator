095a5cc11a9b005e9b82e115adb3fb84729585d8
rohitprocode <rohit990797@gmail.com>
2023-09-16 23:37:50 +0530

    Next Level design

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("This is my Script");
+
+let result = {
+        "tag": "",
+        "free": true,
+        "role": false,
+        "user": "rohit0practice",
+        "email": "rohit0practice@gmail.com",
+        "score": 0.64,
+        "state": "deliverable",
+        "domain": "gmail.com",
+        "reason": "valid_mailbox",
+        "mx_found": true,
+        "catch_all": null,
+        "disposable": false,
+        "smtp_check": true,
+        "did_you_mean": "",
+        "format_valid": true
+      }
+
+      
