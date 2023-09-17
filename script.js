360febc979ca156682ace6eb8b51ed17b2f48280
rohitprocode <rohit990797@gmail.com>
2023-09-18 00:32:36 +0530

    Output changes and Javascript test

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-console.log("This is my Script");
+// console.log("This is my Script");
 
-// Input Field Style 
+// // Input Field Style 
 let inputField = document.getElementById("Email");
 
-inputField.addEventListener('input',()=>{
-    inputField.style.borderBottom = `5px solid ${RandomColor()}`;
-    })  
-//Random color function
-function RandomColor(){
-    let a_Color = "0123456789ABCDEF";
-    let Color = "#";
-    for(let i = 0; i < 6; i++){ 
-    Color += a_Color[Math.floor(Math.random() * 16)];
-    }
-    return Color.toString();
+let result = {
+  "tag": "",
+  "free": true,
+  "role": false,
+  "user": "rohit0practice",
+  "email": "rohit0practice@gmail.com",
+  "score": 0.64,
+  "state": "deliverable",
+  "domain": "gmail.com",
+  "reason": "valid_mailbox",
+  "mx_found": true,
+  "catch_all": null,
+  "disposable": false,
+  "smtp_check": true,
+  "did_you_mean": "",
+  "format_valid": true
 }
 
+let str ;
+for(key of Object.keys(result)){
+    str += `<div>${key} : ${result[key]}</div>`; 
+}
+document.getElementById('disp').innerHTML = str; 
+console.log(str)
 
-//   function setRandomColor() {
-//     $("#colorpad").css("background-color", getRandomColor());
-//   }
-// let result = {
-//         "tag": "",
-//         "free": true,
-//         "role": false,
-//         "user": "rohit0practice",
-//         "email": "rohit0practice@gmail.com",
-//         "score": 0.64,
-//         "state": "deliverable",
-//         "domain": "gmail.com",
-//         "reason": "valid_mailbox",
-//         "mx_found": true,
-//         "catch_all": null,
-//         "disposable": false,
-//         "smtp_check": true,
-//         "did_you_mean": "",
-//         "format_valid": true
-//       }
+inputField.addEventListener('input', ()=>{
+    inputField.style.borderBottom = `5px solid ${RandomColor()}`;
+})
 
+
+
+
+
+//Random color function
+function RandomColor(){
+  let a_Color = "0123456789ABCDEF";
+  let Color = "#";
+  for(let i = 0; i < 6; i++){ 
+  Color += a_Color[Math.floor(Math.random() * 16)];
+  }
+  return Color.toString();
+}
+
+function setRandomColor() {
+  $("#colorpad").css("background-color", getRandomColor());
+}
